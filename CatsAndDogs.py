d07ff8823bb7f99fae5ca428edb4cf093a315a14
vineetgupta086 <vineetgupta086@gmail.com>
2021-06-30 21:35:20 +0530

    input update

--- CatsAndDogs.py
+++ CatsAndDogs.py
@@ -9,24 +9,23 @@ while True:
 
     """Importing Image and Resizing
     """
-    image_name = input('Image Name:')
-    if image_name == '0':
+    ImageLoc = input('Enter image Path:')
+    if ImageLoc == '0':
         break
-
-    image_loc = f"data/{image_name}.jpg"
+    
     try:
-        image = plt.imread(image_loc)
+        Image = plt.imread(ImageLoc)
     except ValueError:
-        print(f"No such file or directory:{image_loc}")
-    resized_image = Resize(image)
+        print(f"No such file or directory:{ImageLoc}")
+    ResizedImg = Resize(Image)
 
     """Model and Predictions  
     """
     MyModel = keras.models.load_model('model.h5')
-    prediction = MyModel.predict(np.array([resized_image]))
+    prediction = MyModel.predict(np.array([ResizedImg]))
     animal = Label(prediction)
 
     """Show Output
     """
-    ShowIm(image = image, label = animal)
+    ShowIm(image = Image, label = animal)
 print("Program Ended.","="*20)
